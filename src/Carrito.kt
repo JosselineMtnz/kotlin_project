3509dba4dd2cae4a8f24656f68ec7f1e12a142b5
Esmeralda <jemh.1199@gmail.com>
2025-02-13 19:51:15 -0600

    Initial commit

---
+++ src/Carrito.kt
@@ -0,0 +1,61 @@
+//Clase para carrito de compras
+class Carrito {
+    private val productosEnCarrito = mutableListOf<ProductoCarrito>()
+
+    //Se agrega un producto
+    fun agregarProducto(producto: Producto, cantidad: Int) {
+        if (producto.cantidadDisponible >= cantidad) {
+            producto.cantidadDisponible -= cantidad
+            val productoCarrito = ProductoCarrito(producto, cantidad)
+            productosEnCarrito.add(productoCarrito)
+            println("Producto agregado: ${productoCarrito.nombre} - Cantidad: $cantidad")
+        } else {
+            println("No hay suficiente cantidad de ${producto.nombre} en inventario.")
+        }
+    }
+
+    //Elimina
+    fun eliminarProducto(nombre: String) {
+        val producto = productosEnCarrito.find { it.nombre == nombre }
+        if (producto != null) {
+            productosEnCarrito.remove(producto)
+            producto.producto.cantidadDisponible += producto.cantidad
+            println("Producto eliminado: ${producto.nombre}")
+        } else {
+            println("Producto no encontrado en el carrito.")
+        }
+    }
+
+    //Muestra
+    fun mostrarCarrito() {
+        var total = 0.0
+        println("\nCarrito de Compras:")
+        for (producto in productosEnCarrito) {
+            val precioTotal = producto.cantidad * producto.precio
+            total += precioTotal
+            println("${producto.nombre} - ${producto.cantidad} x ${producto.precio} = $precioTotal")
+        }
+        println("Total del carrito: $total")
+    }
+
+    //Calcula el total
+    fun totalCarrito(): Double {
+        return productosEnCarrito.sumOf { it.precio * it.cantidad }
+    }
+
+    //Genera la factura
+    fun generarFactura() {
+        val total = totalCarrito()
+        println("\nFactura:")
+        for (producto in productosEnCarrito) {
+            println("${producto.nombre} - ${producto.cantidad} x ${producto.precio} = ${producto.cantidad * producto.precio}")
+        }
+        println("Total General: $total")
+    }
+}
+
+//Clase para representar un Producto en el Carrito
+data class ProductoCarrito(val producto: Producto, val cantidad: Int) {
+    val nombre get() = producto.nombre
+    val precio get() = producto.precio
+}
---
+++ src/Producto.kt
@@ -0,0 +1,2 @@
+//Clase sobre un producto, nombre, precio y cantidad
+data class Producto(val nombre: String, val precio: Double, var cantidadDisponible: Int)
